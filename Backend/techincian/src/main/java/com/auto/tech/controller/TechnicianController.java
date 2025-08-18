9ab73e1bfd544a581edb7ec676aa82de0507ccb4
Bhanu Prasad Kommula <bhanuprasadkommula98@gmail.com>
2025-08-18 13:37:52 -0500

    deploy test ready

--- Backend/techincian/src/main/java/com/auto/tech/controller/TechnicianController.java
+++ Backend/techincian/src/main/java/com/auto/tech/controller/TechnicianController.java
@@ -10,7 +10,6 @@ import java.util.ArrayList;
 import java.util.stream.Collectors;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
